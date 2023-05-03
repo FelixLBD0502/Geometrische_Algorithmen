c5b8bb3758106cda530a12f82bfc2526526de866
FelixLBD0502 <felix.gillrath@lbd.de>
2023-05-03 14:03:25 +0200

    Neues Netzwerk mit Linestrings.

--- netze/geo_netz.py
+++ netze/geo_netz.py
@@ -18,36 +18,39 @@ lines = [
     LineString([(3, 0), (3, 1)])
 ]
 
-# Finde die Schnittpunkte der Linestrings
-points = []
-for i in range(len(lines)):
-    for j in range(i+1, len(lines)):
-        if lines[i].intersects(lines[j]):
-            point = lines[i].intersection(lines[j])
-            if isinstance(point, Point):
-                points.append(point)
+def create_graph(lines):
+    # Finde die Schnittpunkte der Linestrings
+    points = []
+    for i in range(len(lines)):
+        for j in range(i+1, len(lines)):
+            if lines[i].intersects(lines[j]):
+                point = lines[i].intersection(lines[j])
+                if isinstance(point, Point):
+                    points.append(point)
 
-# Erstelle ein Graph-Objekt
-G = nx.Graph()
+    # Erstelle ein Graph-Objekt
+    G = nx.Graph()
 
-# Füge die Knoten hinzu
-for i, point in enumerate(points):
-    G.add_node(i+1, pos=point.coords[0])
+    # Füge die Knoten hinzu
+    for i, point in enumerate(points):
+        G.add_node(i+1, pos=point.coords[0])
 
-# Füge die Kanten hinzu
-for i, line in enumerate(lines):
-    for j, point in enumerate(points):
-        if point == line.coords[0] or point == line.coords[-1]:
-            G.add_edge(j+1, j+2)
+    # Füge die Kanten hinzu
+    for i, line in enumerate(lines):
+        for j, point in enumerate(points):
+            if point == line.coords[0] or point == line.coords[-1]:
+                G.add_edge(j+1, j+2)
 
-# Entferne Knoten ohne Position
-for node in list(G.nodes):
-    if 'pos' not in G.nodes[node]:
-        G.remove_node(node)
+    # Entferne Knoten ohne Position
+    for node in list(G.nodes):
+        if 'pos' not in G.nodes[node]:
+            G.remove_node(node)
 
-# Zeige den Graphen mit matplotlib an und speichere ihn als PDF-Datei
-pos = nx.get_node_attributes(G, 'pos')
-nx.draw(G, pos)
-nx.draw_networkx_edges(G, pos, edgelist=[(i+1, j+1) for i in range(len(points)) for j in range(i+1, len(points))], edge_color='red')
-plt.savefig("geo_netz.pdf")
-plt.show()
+    # Zeige den Graphen mit matplotlib an (und speichere ihn als PDF-Datei)
+    pos = nx.get_node_attributes(G, 'pos')
+    nx.draw(G, pos)
+    nx.draw_networkx_edges(G, pos, edgelist=[(i+1, j+1) for i in range(len(points)) for j in range(i+1, len(points))], edge_color='red')
+    plt.show()
+
+
+create_graph(lines)
